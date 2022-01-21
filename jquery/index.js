611076bbcdab85b157cde3f85c5cc94c06320c8e
ravig1729 <94467990+ravig1729@users.noreply.github.com>
2022-01-21 10:55:39 +0530

    jquery commit

---
+++ jquery/index.js
@@ -0,0 +1,34 @@
+console.log("jquery");
+
+
+$("button").click(function(){
+  $("h1").toggle()
+});
+
+$("button").click(function(){
+    $("p").show()
+    $("p").hide()
+  });
+
+// $("button").click(function(){
+//     //code goes here
+//     $("p").toggle()
+// })
+
+// by class name
+// $(".btn-click").click(function(){
+//     //code goes here
+//     $("p").toggle()
+// })
+
+//by id
+// $("#btn").click(function(){
+//     //code goes here
+//     $("p").toggle()
+// })
+
+//by this keyword
+// $("button").click(function(){
+//     //code goes here
+//    $(this).hide()
+// })
